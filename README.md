9c2bcf0a656802242389a44ec942258eedb2576f
Henry Lee <henryleeworld@gmail.com>
2024-05-06 00:11:33 +0800

    feat:升級至 laravel 11

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Laravel 10 提供特定國家重要節日
+# Laravel 11 提供特定國家重要節日
 
 引入 dansoppelsa 的 laravel-carbon-macros 套件來擴增提供特定國家重要節日，節慶通常有其特殊的文化背景，節慶的儀式也有其特殊的意義。
 
@@ -16,11 +16,15 @@ $ composer install
 ```sh
 $ php artisan key:generate
 ```
+- 執行 __Artisan__ 指令的 __migrate__ 來執行所有未完成的遷移。
+```sh
+$ php artisan migrate
+```
 - 在瀏覽器中輸入已定義的路由 URL 來訪問，例如：http://127.0.0.1:8000。
 - 你可以經由 `/date/holiday/` 來進行日期是否為特定國家重要節日。
 
 ----
 
 ## 畫面截圖
-![](https://i.imgur.com/y3y57CL.png)
+![](https://i.imgur.com/n2dWajo.png)
 > 重要節日是時程安排依據，以便看準日期與時間訂立計劃
--- app/Http/Controllers/DateController.php
+++ app/Http/Controllers/DateController.php
@@ -9,9 +9,9 @@ class DateController extends Controller
     public function isHoliday() 
     {
         $day = Carbon::parse(Carbon::now()->format('Y') . '-12-25');
-        echo Carbon::now()->format('Y') . ' 年 12 月 25 日是否為退伍軍人節：' . ($day->isVeteransDay() ? '是' : '否') . PHP_EOL;
-        echo Carbon::now()->format('Y') . ' 年 12 月 25 日是否為美國感恩節：' . ($day->isAmericanThanksgiving() ? '是' : '否') . PHP_EOL;
-        echo Carbon::now()->format('Y') . ' 年 12 月 25 日是否為聖誕節：' . ($day->isChristmasDay() ? '是' : '否') . PHP_EOL;
-        echo Carbon::now()->format('Y') . ' 年 12 月 25 日是否為元旦：' . ($day->isNewYearsDay() ? '是' : '否') . PHP_EOL;
+        echo __('Is December 25, :year Veterans Day:', ['year' => Carbon::now()->format('Y')]) . ($day->isVeteransDay() ? __('Yes') : __('No')) . PHP_EOL;
+        echo __('Is December 25, :year Thanksgiving:', ['year' => Carbon::now()->format('Y')]) . ($day->isAmericanThanksgiving() ? __('Yes') : __('No')) . PHP_EOL;
+        echo __('Is December 25, :year Christmas:', ['year' => Carbon::now()->format('Y')]) . ($day->isChristmasDay() ? __('Yes') : __('No')) . PHP_EOL;
+        echo __('Is December 25, :year New Year:', ['year' => Carbon::now()->format('Y')]) . ($day->isNewYearsDay() ? __('Yes') : __('No')) . PHP_EOL;
     }
 }
